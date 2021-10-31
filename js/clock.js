9bd279b5ccf382b50c979c99927ef90d8c9e202c
moonyt97 <lkjh262@naver.com>
2021-10-31 10:19:24 +0900

    Update clock.js

--- js/clock.js
+++ js/clock.js
@@ -1,15 +1,15 @@
 const clock = document.querySelector("h2#clock");
 
-function getClock(){
+function getClock() {
   const date = new Date();
-  const hours = String(date.getHours()).padStart(2,"0");
-  const minutes = String(date.getMinutes()).padStart(2,"0");
-  const seconds = String(date.getSeconds()).padStart(2,"0");
-  const days = ["Mon","Tue","Wed","Thu","Fri","Sat","Sun"]
-  const day = days[date.getDay()-1];
+  const hours = String(date.getHours()).padStart(2, "0");
+  const minutes = String(date.getMinutes()).padStart(2, "0");
+  const seconds = String(date.getSeconds()).padStart(2, "0");
+  const days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+  const day = days[date.getDay()];
   clock.innerText = `${hours}:${minutes}:${seconds} ${day}`;
   console.log(clock);
 }
 
 getClock();
-setInterval(getClock,1000);
+setInterval(getClock, 1000);
